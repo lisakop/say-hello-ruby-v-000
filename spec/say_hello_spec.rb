9d8c9b326ba9e0d0b90283649b312d197a442769
Lisa Koperski <lisa_koperski_junk@yahoo.com>
2017-10-06 23:31:06 +0000

    automatically backed up by learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -6,12 +6,12 @@ end
 
 describe "say_hello" do
 
-  it 'accepts an argument of a #{name} and prints out Hello with that #{name}' do
+  it 'accepts an argument of a + name + and prints out Hello with that + name' do
     expect($stdout).to receive(:puts).with("Hello Kent Beck!")
     say_hello("Kent Beck")
   end
 
-  it 'defaults to Ruby Programmer when no #{name} is passed in' do
+  it 'defaults to Ruby Programmer when no + name + is passed in' do
     expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
     say_hello()
   end
